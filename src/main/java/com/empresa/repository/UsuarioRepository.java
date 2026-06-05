a7d4e47d768f0a372d2ff0b6b49eb03d966f8919
JofferHomez <jofferh@hamalsolutions.com>
2026-06-04 20:30:37 -0500

    Add UsuarioRepository

---
+++ src/main/java/com/empresa/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.empresa.repository;
+
+import com.empresa.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    boolean existsByEmail(String email);
+}

7305ed02515e66ddbe1abce7aee53b16a5332534
JofferHomez <jofferh@hamalsolutions.com>
2026-06-04 20:16:49 -0500

    Add Categoria entity

---
+++ src/main/java/com/empresa/entity/Categoria.java
@@ -0,0 +1,29 @@
+package com.empresa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categorias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @ManyToMany(mappedBy = "categorias", fetch = FetchType.LAZY)
+    private Set<Producto> productos = new HashSet<>();
+}

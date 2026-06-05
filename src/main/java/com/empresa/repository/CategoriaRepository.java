bd099249dd5a5baf2cea7f8bb870ceb96459481b
JofferHomez <jofferh@hamalsolutions.com>
2026-06-04 20:41:52 -0500

    Add CategoriaRepository

---
+++ src/main/java/com/empresa/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.empresa.repository;
+
+import com.empresa.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByNombre(String nombre);
+}

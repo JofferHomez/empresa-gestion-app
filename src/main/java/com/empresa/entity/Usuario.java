6437ba60baa472c973620319dd82d0a0a4e2ea71
JofferHomez <jofferh@hamalsolutions.com>
2026-06-04 20:14:12 -0500

    Add Usuario entity

---
+++ src/main/java/com/empresa/entity/Usuario.java
@@ -0,0 +1,35 @@
+package com.empresa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role rol;
+
+    @Column(nullable = false)
+    private Boolean activo = true;
+
+    public enum Role {
+        ADMIN, EXTERNAL
+    }
+}
